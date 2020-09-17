ba08696d38d3b468a5f2db5d096f2ae14e957934
Muhammad Faisal Adi Soesatyo <faisaladi27@gmail.com>
2020-09-17 22:00:12 +0700

    fix another security issues

--- story/settings.py
+++ story/settings.py
@@ -125,6 +125,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
