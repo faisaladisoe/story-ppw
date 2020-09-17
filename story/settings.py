e1ff1f0343bf86b2011f199292ae803e93468b35
Muhammad Faisal Adi Soesatyo <faisaladi27@gmail.com>
2020-09-17 23:35:04 +0700

    fix minor issue

--- story/settings.py
+++ story/settings.py
@@ -25,7 +25,7 @@ with open('./secret_key.txt') as f:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['faisal-ppw.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['faisal-ppw.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
