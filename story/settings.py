cf6eda6d44e239573ecf6c17d919e99a33057c54
Muhammad Faisal Adi Soesatyo <faisaladi27@gmail.com>
2020-09-17 23:26:12 +0700

    fix some code in settings.py

--- story/settings.py
+++ story/settings.py
@@ -124,9 +124,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# Manually added
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
--- story1/views.py
+++ story1/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'story1/index.html');
+    return render(request, 'story1/index.html')
