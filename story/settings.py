b02fbd8debc7133d211ca2e116269413023677b4
Muhammad Faisal Adi Soesatyo <faisaladi27@gmail.com>
2020-09-20 16:58:25 +0700

    fix the settings.py

--- story/settings.py
+++ story/settings.py
@@ -14,6 +14,16 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 load_dotenv()
+from dotenv import load_dotenv
+load_dotenv()
+
+# OR, the same with increased verbosity
+load_dotenv(verbose=True)
+
+# OR, explicitly providing path to '.env'
+from pathlib import Path  # Python 3.6+ only
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
