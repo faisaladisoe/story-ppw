a3e144497877fefdbc1f216e1c321fc55de7a352
Muhammad Faisal Adi Soesatyo <faisaladi27@gmail.com>
2020-09-29 15:22:24 +0700

    add link to another page

--- story1/views.py
+++ story1/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'story1/index.html')
+
+def copyright(request):
+    return render(request, 'story1/copyright.html')
