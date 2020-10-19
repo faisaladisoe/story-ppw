9e86918c6c45e65db53238ff8548b0f6544f4534
Muhammad Faisal Adi Soesatyo <faisaladi27@gmail.com>
2020-10-19 23:52:08 +0700

    clear all the comments

--- story1/admin.py
+++ story1/admin.py
@@ -4,5 +4,3 @@ from django.contrib import admin
 from . import models
 
 admin.site.register(models.Course)
-# admin.site.register(models.Event)
-# admin.site.register(models.Visitor)
--- story1/models.py
+++ story1/models.py
@@ -33,32 +33,4 @@ class Course(models.Model):
 
     def __str__(self):
         return '{}'.format(self.course_name)
-
-# class Event(models.Model):
-#     event = models.CharField(max_length = 50, null = True)
-#     brief_description = models.CharField(max_length = 200, null = True)
-#     complete_description = models.TextField(max_length = 1000, null = True)
-
-#     def __str__(self):
-#         return '{}'.format(self.event)
-
-# class Visitor(models.Model):
-#     visitor_name = models.CharField(max_length = 75, null = True)
-#     visitor_age = models.PositiveIntegerField(null = True)
-#     ID_TYPE = (
-#         ('KTM', 'KTM'),
-#         ('KTP', 'KTP'),
-#         ('SIM', 'SIM'),
-#         ('Passport', 'Passport'),
-#     )
-#     id_type = models.CharField(
-#         max_length = 10 ,
-#         choices = ID_TYPE,
-#         default = 'KTM',
-#         null = True
-#     )
-#     identity_number = models.CharField(max_length = 75, null = True)
-#     event = models.ForeignKey(Event, on_delete = models.CASCADE)
-
-#     def __str__(self):
-#         return '{}'.format(self.visitor_name)
+    
