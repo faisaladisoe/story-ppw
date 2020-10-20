567970f931356ae133825ead418b0a1927033672
Muhammad Faisal Adi Soesatyo <faisaladi27@gmail.com>
2020-10-20 17:00:32 +0700

    add another tests in test_views.py

--- story6/tests/test_views.py
+++ story6/tests/test_views.py
@@ -1,6 +1,6 @@
 from http import HTTPStatus
 from django.test import TestCase, Client
-from django.urls import resolve
+from django.urls import resolve, reverse
 from story6.models import Event, Visitor
 
 # Create your tests here.
@@ -61,5 +61,41 @@ class story6ViewsTest(TestCase):
     def test_deleteVisitor_url_GET(self):
         response = self.client.get(self.deleteVisitor_url)
         self.assertTemplateUsed(response, 'story6/deleteVisitor.html')
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_add_events_url_POST(self):
+        response = self.client.post(self.addEvent_url, {
+            'event' : 'Campus Fair',
+            'brief_description' : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            'complete_description' : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
+        }, follow = True)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+    
+    def test_delete_events_url_POST(self):
+        response = self.client.post(self.deleteEvents_url, follow = True)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+    
+    def test_register_visitor_url_POST(self):
+        response = self.client.post(self.registerVisitor_url, {
+            'visitor_name' : 'Muhammad Faisal Adi Soesatyo',
+            'visitor_age' : 19,
+            'id_type' : 'SIM',
+            'identity_number' : '123456789',
+            'event' : self.test_event
+        }, follow = True)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+    
+    def test_delete_visitor_url_POST(self):
+        response = self.client.post(self.deleteVisitor_url, follow = True)
+        self.assertEquals(response.status_code, HTTPStatus.OK)
+    
+    def test_update_visitor_url_POST(self):
+        response = self.client.post(self.updateVisitor_url, {
+            'visitor_name' : 'Muhammad Faisal Adi Soesatyo',
+            'visitor_age' : 19,
+            'id_type' : 'SIM',
+            'identity_number' : '123456789',
+            'event' : self.test_event
+        }, follow = True)
         self.assertEqual(response.status_code, HTTPStatus.OK)
     
