c4363bdb46484ef90b4bd73ea1b7a47209f61cdb
Muhammad Faisal Adi Soesatyo <faisaladi27@gmail.com>
2020-11-24 12:50:29 +0700

    Implement the core of story 7

---
+++ story7/static/story7/js/accordion.js
@@ -0,0 +1,20 @@
+$(function () {
+    $("#accordion")
+        .accordion({
+            header: "> div > h3",
+            collapsible: true
+        })
+        .sortable({
+            handle: "h3",
+            placeholder: "ui-state-highlight",
+            placeholder: "ui-state-highlight",
+            over: function (event, ui) {
+                $('#accordion').accordion({ active: false });
+            },
+            stop: function (event, ui) {
+                // IE doesn't register the blur when sorting
+                // so trigger focusout handlers to remove .ui-state-focus
+                ui.item.children("h3").triggerHandler("focusout");
+            }
+        });
+});
--- story7/views.py
+++ story7/views.py
@@ -2,4 +2,24 @@ from django.shortcuts import render
 
 # Create your views here.
 def accordion(request):
-    return render(request, 'story7/accordion.html')
+    content = {
+        'data': [
+            {
+                'title': 'My Activity',
+                'topic': 'Learning new stuff, Pursue my ambition, Shape my dream'
+            },
+            {
+                'title': 'Experience',
+                'topic': 'I have no experience both in organisation and comittee'
+            },
+            {
+                'title': 'Achievement',
+                'topic': 'I have no achievement at all'
+            },
+            {
+                'title': 'Dream',
+                'topic': 'I want to be a CEO and an investor'
+            }
+        ]
+    }
+    return render(request, 'story7/accordion.html', content)
